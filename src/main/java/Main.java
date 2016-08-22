e3da19bbe12e721e291e966eca5eb2ab0f7a3f9c
Ben Sterrett <raynjamin@gmail.com>
2016-08-22 12:11:31 -0400

    Adding post request for coffee shops

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,39 +13,41 @@ import spark.template.freemarker.FreeMarkerEngine;
 import spark.ModelAndView;
 import static spark.Spark.get;
 
-import com.heroku.sdk.jdbc.DatabaseUrl;
 
 public class Main {
 
-  private static ArrayList<CoffeeShop> shops = new ArrayList<>();
+    private static ArrayList<CoffeeShop> shops = new ArrayList<>();
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        port(Integer.valueOf(System.getenv("PORT")));
+        staticFileLocation("/public");
 
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
 
+        get("/hello", (req, res) -> "Hello World");
 
-
-    port(Integer.valueOf(System.getenv("PORT")));
-    staticFileLocation("/public");
-
-      before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
-
-    get("/hello", (req, res) -> "Hello World");
-
-    get("/", (request, response) -> {
+        get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("message", "Hello World!");
 
             return new ModelAndView(attributes, "index.ftl");
         }, new FreeMarkerEngine());
 
-    get("/coffee-shops", (req, res) -> {
-        shops.add(new CoffeeShop("Not Just Coffee", 5, 3));
+        get("/coffee-shops", (req, res) -> {
+            Gson gson = new Gson();
 
-        Gson gson = new Gson();
+            return gson.toJson(shops);
+        });
 
-        return gson.toJson(shops);
-    });
+        post("/coffee-shops", (req, res) -> {
+            String name = req.params("name");
+            String rating = req.params("rating");
+            String price = req.params("price");
 
-  }
+            shops.add(new CoffeeShop(name, Integer.valueOf(rating), Integer.valueOf(price)));
+
+            return 200;
+        });
+    }
 
 }
