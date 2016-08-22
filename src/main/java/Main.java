207393b0462105995d3956ac499627d74bfe2c65
Ben Sterrett <raynjamin@gmail.com>
2016-08-22 11:33:05 -0400

    Reverting the ignorance of the port env variable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,7 +21,7 @@ public class Main {
 
   public static void main(String[] args) {
 
-    port(5000);
+    port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
     get("/hello", (req, res) -> "Hello World");
